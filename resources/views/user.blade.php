6d77e5d2784686ec7f6c8a7d452aaa9d72bfa8b7
Sanjay Tiwari <stiwari@g.harvard.edu>
2015-10-17 20:13:57 -0500

    number validator

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -65,7 +65,7 @@
 
         <h1>User Generator</h1>
 
-      	<form method="POST" name="user-gen" action="/user-generator" accept-charset="UTF-8" >
+      	<form method="POST" name="user-gen" action="/user-generator" onsubmit="return validateForm()" accept-charset="UTF-8" >
       		<label for="users">How many users?</label>
           <input maxlength="2" name="users" type="number" value={{(isset($num_users))?$num_users:2}} id="users"> (Max: 99)
       		<br>
